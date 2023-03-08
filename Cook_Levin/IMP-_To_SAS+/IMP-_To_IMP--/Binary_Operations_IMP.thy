c631068ad9bfb988cee4f26acc7f913ef54444c5
Adem Rimpapa <rimpapa@in.tum.de>
2023-03-08 15:56:03 +0100

    Added new file Binary_Operations_IMP.thy

---
+++ Cook_Levin/IMP-_To_SAS+/IMP-_To_IMP--/Binary_Operations_IMP.thy
@@ -0,0 +1,13 @@
+theory Binary_Operations_IMP
+  imports
+    Primitives_IMP_Minus
+    Binary_Arithmetic_IMP
+    IMP_Minus_To_IMP_Minus_Minus_State_Translations_IMP
+    Binary_Operations_Nat
+    IMP_Minus.Com
+    Utilities
+begin
+
+unbundle IMP_Minus_Minus_Com.no_com_syntax
+
+end
