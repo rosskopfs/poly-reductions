19af94dcd2cd166f61f0bd5649f7ce53af646669
Lukas Stevens <mail@lukas-stevens.de>
2023-04-08 13:36:45 +0200

    feat(Compile_Nat) use abs_def and refactor index
    - Use abs_def to deal with arguments for a definitional constant
    - Index tc_ast s.t. indices increase from left to right

--- Cook_Levin_IMP/IMP-_To_SAS+/Compile_Nat.thy
+++ Cook_Levin_IMP/IMP-_To_SAS+/Compile_Nat.thy
@@ -13,30 +13,41 @@ ML \<open>
   | Copy of (indexname * indexname)
   | IfNeqZero of (indexname * (indexname * imp * imp))
 
-  fun tc_ast_of_term index t =
+  fun tc_ast_of_term t =
     let
       fun fold_step a (index, args_ast) =
-            let val (index', a_ast) = tc_ast_of_term index a
+            let val (index', a_ast) = index_tc_ast index a
             in (index', a_ast :: args_ast) end
+      and index_tc_ast index (App (_, f, args)) =
+        let
+          val (index', rev_args') = fold fold_step args (index, [])
+        in
+          (index' + 1, App (index', f, rev rev_args'))
+        end
+        | index_tc_ast index (If (_, c, e1, e2)) =
+        let
+          val (index', [e2', e1', c']) = fold fold_step [c, e1, e2] (index, [])
+        in
+          (index' + 1, If (index', c', e1', e2'))
+        end
+        
+
+      fun build_tc_ast t =
+        case Term.strip_comb t of
+          (\<^Const>\<open>HOL.If \<^typ>\<open>nat\<close>\<close>
+          , [\<^Const>\<open>HOL.Not for \<open>\<^Const>\<open>HOL.eq \<^typ>\<open>nat\<close> for c \<open>\<^Const>\<open>Groups.zero \<^typ>\<open>nat\<close>\<close>\<close>\<close>\<close>\<close>, e1, e2]) =>
+            let
+              val [c_ast, e1_ast, e2_ast] = map build_tc_ast [c, e1, e2]
+            in
+              If (~1, c_ast, e1_ast, e2_ast)
+            end
+        | (f, args) => App (~1, f, map build_tc_ast args)
     in
-      case Term.strip_comb t of
-        (\<^Const>\<open>HOL.If \<^typ>\<open>nat\<close>\<close>
-        , [\<^Const>\<open>HOL.Not for \<open>\<^Const>\<open>HOL.eq \<^typ>\<open>nat\<close> for c \<open>\<^Const>\<open>Groups.zero \<^typ>\<open>nat\<close>\<close>\<close>\<close>\<close>\<close>, e1, e2]) =>
-          let
-            val (index', [c_ast, e1_ast, e2_ast]) = fold_rev fold_step [c, e1, e2] (index, [])
-          in
-            (index' + 1, If (index', c_ast, e1_ast, e2_ast))
-          end
-      | (f, args) =>
-          let
-            val (index', args_ast) = fold_rev fold_step args (index, [])
-          in
-            (index' + 1, App (index, f, args_ast))
-          end
+      t |> build_tc_ast |> index_tc_ast 0 |> snd
     end
 
-  fun reg_of_term _ (Var ((s, idx), _)) = (s, idx)
-    | reg_of_term idx (Const (s, _)) = (s, idx)
+  fun reg_of_term idx (Const (s, _)) = (s, idx)
+    | reg_of_term idx (Free (s, _)) = (s, idx)
 
   fun imp_of_tc_ast (App (index, t, args)) =
     let
@@ -62,11 +73,11 @@ ML \<open>
       ))
     end
 
-  fun let_of_imp imp =
+  fun let_of_imp (ret_reg, imp) =
     let
       fun reg_of_indexname (n, i) =
         String.map (fn c => if c = #"." then #"_" else c) n
-        ^ "_" ^ @{make_string} i
+        ^ "_" ^ Int.toString i
       val natT = \<^typ>\<open>nat\<close>
       fun reg_var_of_indexname n = Free (reg_of_indexname n, natT)
       
@@ -85,9 +96,11 @@ ML \<open>
         in
           \<^Const>\<open>Let natT natT for \<open>list_comb (f, args_var)\<close> \<open>lambda reg_var target\<close>\<close>
         end
+        (* Ignoring target should work since we can't sequence if in HOL *)
         | go target (IfNeqZero (ret_reg, (cond_reg, e1, e2))) =
         let
-          val (ret_reg_var, cond_reg_var) = apply2 reg_var_of_indexname (ret_reg, cond_reg)
+          val (ret_reg_var, cond_reg_var) =
+            apply2 reg_var_of_indexname (ret_reg, cond_reg)
           val (e1_let, e2_let) = apply2 (go ret_reg_var) (e1, e2)
           val condt = \<^Const>\<open>HOL.Not for
             \<^Const>\<open>HOL.eq natT for cond_reg_var \<^Const>\<open>Groups.zero natT\<close>\<close>\<close>
@@ -96,25 +109,27 @@ ML \<open>
         end
         
     in
-      go \<^Const>\<open>id natT\<close> imp
+      go (reg_var_of_indexname ret_reg) imp
     end
 
-  fun tc_ast_of_def def =
-    tc_ast_of_term (def |> Thm.cprop_of |> Thm.maxidx_of_cterm) (def |> Thm.rhs_of |> Thm.term_of)
-    |> snd
-  fun imp_of_def def = def |> tc_ast_of_def |> imp_of_tc_ast |> snd
-  fun let_of_def def = def |> imp_of_def |> let_of_imp
-
-  fun define_let_of_def def binding =
+  fun define_let_of_def def binding lthy =
     let
-      val let_def = let_of_def def
-                    |> Term.close_schematic_term
-                    |> Term.map_aterms (fn t => case t of Var (n, T) => Free (fst n, T) | _ => t)
+      val (args, def_body) =
+        Local_Defs.abs_def_rule lthy def
+        |> Thm.rhs_of |> Thm.term_of |> Term.strip_abs
+      val let_def =
+           def_body
+        |> curry Term.subst_bounds (map Free args |> rev)
+        |> tc_ast_of_term
+        |> imp_of_tc_ast
+        |> let_of_imp
+        |> fold_rev Term.absfree args (* Could go wrong if there are fixed variables in the context *)
     in
-      snd o Local_Theory.define ((binding, NoSyn), ((Thm.def_binding binding, []), let_def))
+      Local_Theory.define ((binding, NoSyn), ((Thm.def_binding binding, []), let_def)) lthy
+      |> snd
     end
 \<close>
-definition "test (x :: nat) y \<equiv> if x + y \<noteq> 0 then x else 0"
+definition "test (x :: nat) (y :: nat) \<equiv> if x + y \<noteq> 0 then if y \<noteq> 0 then y else x else 0"
 
 local_setup \<open>
   define_let_of_def @{thm test_def} \<^binding>\<open>test_let\<close>
