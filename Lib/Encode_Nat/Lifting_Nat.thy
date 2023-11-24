d5e3d21f37019dc7d8e2d19e077e62ecb6467631
Lukas Stevens <mail@lukas-stevens.de>
2023-11-24 19:53:04 +0100

    feat(Lib/Encode_Nat/Lifting_Nat) use typeclasses

--- Lib/Encode_Nat/Lifting_Nat.thy
+++ Lib/Encode_Nat/Lifting_Nat.thy
@@ -4,37 +4,35 @@ begin
 
 section \<open>Lifting to \<^typ>\<open>nat\<close>\<close>
 
-definition cr_nat :: "('a \<Rightarrow> nat) \<Rightarrow> nat \<Rightarrow> 'a \<Rightarrow> bool" where
-  "cr_nat Abs_nat \<equiv> (\<lambda>n l. n = Abs_nat l)"
+class lift_nat =
+  fixes Abs_nat :: "'a \<Rightarrow> nat"
+  fixes Rep_nat :: "nat \<Rightarrow> 'a"
+  assumes Rep_nat_Abs_nat_id[simp]: "\<And>x. Rep_nat (Abs_nat x) = x"
+begin
+
+lemma inj_Abs_nat: "inj Abs_nat"
+  using Rep_nat_Abs_nat_id unfolding inj_def by metis
+
+definition cr_nat :: "nat \<Rightarrow> 'a \<Rightarrow> bool" where
+  "cr_nat \<equiv> (\<lambda>n l. n = Abs_nat l)"
+
+lemma typedef_nat: "type_definition Abs_nat Rep_nat (Abs_nat ` UNIV)"
+  by (unfold_locales) auto
+ 
+lemmas typedef_nat_transfer[OF typedef_nat cr_nat_def, transfer_rule] =
+  typedef_bi_unique typedef_right_unique typedef_left_unique typedef_right_total
+
+lemma cr_nat_Abs_nat[transfer_rule]:
+  "cr_nat (Abs_nat x) x"
+  unfolding cr_nat_def by simp
+
+end
 
 text \<open>
   This locale needs to instantiated for every base type.
   The first axiom probably has to be proved by the user.
   The second axiom is already proved automatically.
 \<close>
-locale type_definition_nat =
-  fixes Abs_nat :: "'a \<Rightarrow> nat"
-  fixes Rep_nat :: "nat \<Rightarrow> 'a"
-
-  assumes inj_Rep_nat: "inj Abs_nat"
-  assumes Abs_nat_Rep_nat_id: "\<And>x. Rep_nat (Abs_nat x) = x"
-begin
-
-lemma typedef_nat: "type_definition Abs_nat Rep_nat (Abs_nat ` UNIV)"
-  apply(unfold_locales)
-    apply(use inj_Rep_nat Abs_nat_Rep_nat_id in auto)
-  done
-
-definition "cr_nat' \<equiv> cr_nat Abs_nat"
-
-lemmas typedef_nat_transfer[OF typedef_nat cr_nat'_def[unfolded cr_nat_def], transfer_rule] =
-  typedef_bi_unique typedef_right_unique typedef_left_unique typedef_right_total
-
-lemma cr_nat_Abs_nat[transfer_rule]:
-  "cr_nat' (Abs_nat x) x"
-  unfolding cr_nat'_def cr_nat_def by simp
-
-end
 
 abbreviation "fstP x \<equiv> fst (prod_decode x)"
 abbreviation "sndP x \<equiv> snd (prod_decode x)"
@@ -42,53 +40,51 @@ abbreviation "sndP x \<equiv> snd (prod_decode x)"
 
 section \<open>Lifting from \<^typ>\<open>bool\<close>\<close>
 
-\<comment> \<open>Provided by the datatype compiler\<close>
-definition Abs_nat_bool :: "bool \<Rightarrow> nat" where
-  "Abs_nat_bool b \<equiv> if b then 1 else 0"
+instantiation bool :: lift_nat
+begin
 
-\<comment> \<open>Provided by the datatype compiler\<close>
-definition Rep_nat_bool :: "nat \<Rightarrow> bool" where
-  "Rep_nat_bool n \<equiv> if n = 0 then False else True"
+definition Abs_nat_bool_def: "Abs_nat b \<equiv> if b then 1 else 0"
 
-global_interpretation nat_bool:
-  type_definition_nat Abs_nat_bool Rep_nat_bool
-  defines cr_nat_bool_raw_def: cr_nat_bool = nat_bool.cr_nat'
-  apply(unfold_locales)
-   apply(auto simp: inj_on_def Abs_nat_bool_def Rep_nat_bool_def)
+definition Rep_nat_bool_def: "Rep_nat n \<equiv> if n = 0 then False else True"
+
+instance 
+  apply (intro_classes)
+   apply (auto simp: Abs_nat_bool_def Rep_nat_bool_def)
   done
-lemmas cr_nat_bool_def = nat_bool.cr_nat'_def[unfolded cr_nat_def]
+
+end
 
 lemma Domainp_nat_bool_rel[transfer_domain_rule]:
-  "Domainp cr_nat_bool = (\<lambda>x. x = 0 \<or> x = 1)"
-  unfolding cr_nat_bool_def Abs_nat_bool_def by auto
+  "Domainp (cr_nat :: _ \<Rightarrow> bool \<Rightarrow> _) = (\<lambda>x. x = 0 \<or> x = 1)"
+  unfolding cr_nat_def Abs_nat_bool_def by auto
 
 context includes lifting_syntax
 begin
 
 \<comment> \<open>Needs to be provided by the datatype compiler\<close>
 lemma cr_nat_bool_True[transfer_rule]:
-  "cr_nat_bool 1 True"
-  unfolding cr_nat_bool_def Abs_nat_bool_def by auto
+  "cr_nat 1 True"
+  unfolding cr_nat_def Abs_nat_bool_def by auto
 
 \<comment> \<open>Needs to be provided by the datatype compiler\<close>
 lemma cr_nat_bool_False[transfer_rule]:
-  "cr_nat_bool 0 False"
-  unfolding cr_nat_bool_def Abs_nat_bool_def by auto
+  "cr_nat 0 False"
+  unfolding cr_nat_def Abs_nat_bool_def by auto
 
 lemma nat_bool_rel_conj[transfer_rule]:
-  "(cr_nat_bool ===> cr_nat_bool ===> cr_nat_bool) min (\<and>)"
-  unfolding cr_nat_bool_def Abs_nat_bool_def
+  "(cr_nat ===> cr_nat ===> cr_nat) min (\<and>)"
+  unfolding cr_nat_def Abs_nat_bool_def
   by (auto simp: rel_fun_def)
 
 lemma nat_bool_rel_disj[transfer_rule]:
-  "(cr_nat_bool ===> cr_nat_bool ===> cr_nat_bool) max (\<or>)"
-  unfolding cr_nat_bool_def Abs_nat_bool_def
+  "(cr_nat ===> cr_nat ===> cr_nat) max (\<or>)"
+  unfolding cr_nat_def Abs_nat_bool_def
   by (auto simp: rel_fun_def)
 
 end
 
 schematic_goal
-  shows "cr_nat_bool ?t (a \<and> (b \<or> c))"
+  shows "cr_nat ?t (a \<and> (b \<or> c))"
   apply transfer_prover_start
        apply transfer_step+
   apply (rule HOL.refl)
@@ -96,12 +92,6 @@ schematic_goal
 
 
 section \<open>Lifting from \<^typ>\<open>'a list\<close>\<close>
-
-\<comment> \<open>Already provided by the datatype compiler\<close>
-fun Rep_nat_list where
-  "Rep_nat_list Rep_nat [] = prod_encode (0, 0)"
-| "Rep_nat_list Rep_nat (x # xs) =
-    prod_encode (1, prod_encode (Rep_nat x, Rep_nat_list Rep_nat xs))"
 
 \<comment> \<open>Already provided by the datatype compiler\<close>
 definition "case_list_nat f g xs \<equiv>
@@ -148,9 +138,18 @@ lemma fstP_lt: "0 < n \<Longrightarrow> fstP n < n"
 lemma sndP_lt: "1 < n \<Longrightarrow> sndP n < n"
   unfolding prod_decode_def using snd_prod_decode_aux_lt[where ?k=0] by simp
 
+instantiation list :: (lift_nat) lift_nat
+begin
+
 \<comment> \<open>Already provided by the datatype compiler\<close>
-function Abs_nat_list where
-  "Abs_nat_list Abs_nat n = case_list_nat [] (\<lambda>x xs. Abs_nat x # Abs_nat_list Abs_nat xs) n"
+fun Abs_nat_list :: "'a list \<Rightarrow> nat" where
+  "Abs_nat_list [] = prod_encode (0, 0)"
+| "Abs_nat_list (x # xs) =
+    prod_encode (1, prod_encode (Abs_nat x, Abs_nat_list xs))"
+
+\<comment> \<open>Already provided by the datatype compiler\<close>
+function Rep_nat_list :: "nat \<Rightarrow> 'a list" where
+  "Rep_nat_list n = case_list_nat [] (\<lambda>x xs. Rep_nat x # Rep_nat_list xs) n"
   by auto
 termination
 proof -
@@ -160,40 +159,21 @@ proof -
     by (metis bot_nat_0.not_eq_extremum fstP_lt le_prod_encode_2 less_add_same_cancel1 not_less_eq
         order_less_le_trans plus_1_eq_Suc prod.exhaust_sel prod_decode_inverse sndP_lt)
   then show ?thesis
-    apply (relation "measure (\<lambda>(Abs_nat, n). size n)")
+    apply (relation "measure (\<lambda>n. size n)")
      apply (auto)
     done
 qed
-declare Abs_nat_list.simps[simp del]
+declare Rep_nat_list.simps[simp del]
 
-\<comment> \<open>User has to provide this to the datatype compiler\<close>
-lemma inj_Rep_nat_list:
-  "inj Rep_nat \<Longrightarrow> inj (Rep_nat_list Rep_nat)"
-  apply(intro injI)
-  subgoal for xs ys
-    apply(induction xs ys rule: list_induct2')
-       apply(auto dest: inj_onD)
-    done
-  done
-
-\<comment> \<open>Already provided by the datatype compiler\<close>
-lemma Abs_nat_list_Rep_nat_list:
-  assumes "\<And>x. Abs_nat (Rep_nat x) = x"
-  shows "Abs_nat_list Abs_nat (Rep_nat_list Rep_nat xs) = xs"
-  using assms
-proof(induction xs)
-  case Nil
-  then show ?case
-    apply(subst Abs_nat_list.simps)
-    apply(auto simp: case_list_nat_def)
-    done
-next
-  case (Cons x xs)
-  then show ?case
-    apply(subst Abs_nat_list.simps)
-    apply(auto simp: case_list_nat_def)
+instance
+proof(intro_classes)
+  show "Rep_nat (Abs_nat xs) = xs" for xs :: "'a list"
+    apply (induction xs)
+     apply (subst Rep_nat_list.simps, simp add: case_list_nat_def)+
     done
 qed
+
+end
 
 fun rev_tr :: "'a list \<Rightarrow> 'a list \<Rightarrow> 'a list" where
   "rev_tr acc [] = acc"
@@ -201,25 +181,9 @@ fun rev_tr :: "'a list \<Rightarrow> 'a list \<Rightarrow> 'a list" where
 
 case_of_simps rev_tr_case_def: rev_tr.simps
 
-context
-  fixes Rep_nat :: "'a \<Rightarrow> nat"
-  fixes Abs_nat :: "nat \<Rightarrow> 'a"
-begin
-
 \<comment> \<open>Can be lifted with Kevin's transport framework\<close>
-definition "rev_tr_nat acc xs \<equiv>
-  Rep_nat_list Rep_nat (rev_tr (Abs_nat_list Abs_nat acc) (Abs_nat_list Abs_nat xs))"
-
-context
-  assumes inj_Rep_nat: "inj Rep_nat"
-  assumes Abs_nat_Rep_nat: "\<And>x. Abs_nat (Rep_nat x) = x"
-begin
-
-global_interpretation nat_list: type_definition_nat "Rep_nat_list Rep_nat" "Abs_nat_list Abs_nat"
-  defines cr_nat_list_raw_def: cr_nat_list = "nat_list.cr_nat'"
-  using inj_Rep_nat_list[OF inj_Rep_nat] Abs_nat_list_Rep_nat_list[OF Abs_nat_Rep_nat]
-  by (unfold_locales)
-lemmas cr_nat_list_def = nat_list.cr_nat'_def[unfolded cr_nat_def]
+definition rev_tr_nat :: "'a::lift_nat itself \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> nat" where
+  "rev_tr_nat _ acc xs \<equiv> (Abs_nat :: 'a list \<Rightarrow> nat) (rev_tr (Rep_nat acc) (Rep_nat xs))"
 
 context includes lifting_syntax
 begin
@@ -230,37 +194,34 @@ begin
   each constructor.
 \<close>
 lemma cr_nat_list_Nil[transfer_rule]:
-  "cr_nat_list (prod_encode (0, 0)) []"
-  unfolding cr_nat_list_def by simp
+  "cr_nat (prod_encode (0, 0)) []"
+  unfolding cr_nat_def by simp
 
 \<comment> \<open>Needs to be provided by the datatype compiler\<close>
 lemma cr_nat_list_Cons[transfer_rule]:
-  "(cr_nat Rep_nat ===> cr_nat_list ===> cr_nat_list)
-    (\<lambda>x xs. prod_encode (1, prod_encode (x, xs))) (#)"
-  unfolding cr_nat_list_def cr_nat_def by (auto simp: rel_fun_def)
+  "(cr_nat ===> cr_nat ===> cr_nat) (\<lambda>x xs. prod_encode (1, prod_encode (x, xs))) (#)"
+  unfolding cr_nat_def by (auto simp: rel_fun_def)
 
 \<comment> \<open>Needs to be provided by the datatype compiler\<close>
 lemma cr_nat_list_case_list[transfer_rule]:
-  "(R ===> (cr_nat Rep_nat ===> cr_nat_list ===> R) ===> cr_nat_list
-  ===> R) case_list_nat case_list"
-  unfolding cr_nat_def cr_nat_list_def case_list_nat_def
+  "(R ===> (cr_nat ===> cr_nat ===> R) ===> cr_nat ===> R) case_list_nat case_list"
+  unfolding cr_nat_def case_list_nat_def
   by (auto simp: rel_fun_def split: prod.splits list.splits)
-
-end
 
 \<comment> \<open>Can be proved with Kevin's transport framework\<close>
 lemma rev_tr_nat_lifting[transfer_rule]:
   includes lifting_syntax
-  shows "(cr_nat_list ===> cr_nat_list ===> cr_nat_list) rev_tr_nat rev_tr"
-  unfolding rev_tr_nat_def cr_nat_list_def
-  using Abs_nat_list_Rep_nat_list[OF Abs_nat_Rep_nat]
+  shows "(cr_nat ===> cr_nat ===> cr_nat) (rev_tr_nat TYPE('a::lift_nat)) (rev_tr :: 'a list \<Rightarrow> _)"
+  unfolding rev_tr_nat_def cr_nat_def
   apply(auto simp: rel_fun_def)
   done
 
+end
+
 schematic_goal rev_tr_nat_synth:
-  assumes [transfer_rule]: "cr_nat_list accn (Abs_nat_list Abs_nat accn)"
-  assumes [transfer_rule]: "cr_nat_list xsn (Abs_nat_list Abs_nat xsn)"
-  shows "cr_nat_list ?t (rev_tr (Abs_nat_list Abs_nat accn) (Abs_nat_list Abs_nat xsn))"
+  assumes [transfer_rule]: "(cr_nat :: nat \<Rightarrow> 'a list \<Rightarrow> bool) accn (Rep_nat accn)"
+  assumes [transfer_rule]: "(cr_nat :: nat \<Rightarrow> 'a list \<Rightarrow> bool) xsn (Rep_nat xsn)"
+  shows "cr_nat ?t ((rev_tr :: 'a::lift_nat list \<Rightarrow> _) (Rep_nat accn) (Rep_nat xsn))"
   apply (subst rev_tr_case_def)
   apply transfer_prover_start
         apply transfer_step+
@@ -268,17 +229,16 @@ schematic_goal rev_tr_nat_synth:
   done
 
 lemma rev_tr_nat_synth_def:
-  assumes "accn = Rep_nat_list Rep_nat acc"
-  assumes "xsn = Rep_nat_list Rep_nat acc"
-  shows "rev_tr_nat accn xsn
-    = case_list_nat accn (\<lambda>x3a. rev_tr_nat (prod_encode (1, prod_encode (x3a, accn)))) xsn"
-  using assms rev_tr_nat_def rev_tr_nat_synth[unfolded cr_nat_list_def]
-  by (metis nat_list.Abs_nat_Rep_nat_id)
+  fixes acc :: "'a::lift_nat list" and xs :: "'a list"
+  assumes "accn = Abs_nat acc"
+  assumes "xsn = Abs_nat xs"
+  shows "rev_tr_nat TYPE('a) accn xsn
+    = case_list_nat accn (\<lambda>x3a. rev_tr_nat TYPE('a) (prod_encode (1, prod_encode (x3a, accn)))) xsn"
+  using assms rev_tr_nat_def rev_tr_nat_synth[unfolded cr_nat_def]
+  using Rep_nat_Abs_nat_id
+  by (smt (z3) case_list_nat_cong)
 
 \<comment> \<open>Final theorem that can be passed to the IMP compiler\<close>
 thm rev_tr_nat_synth_def[unfolded case_list_nat_def]
-end
-
-end
 
 end
