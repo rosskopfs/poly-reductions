21d8b1fe17c76d0f12a62be146ce19d055acd1b7
Jiong Liu <ge65fed@mytum.de>
2024-04-24 12:34:35 +0200

    remove the inth and fix the small problem non-termination in GOTO

--- IMPminus-TM/Def/GOTO.thy
+++ IMPminus-TM/Def/GOTO.thy
@@ -2,18 +2,6 @@ theory GOTO
   imports Main "HOL-IMP.Star" (*"HOL-IMP.Compiler"*)
 begin
 
-(* what does this mean? *)
-declare [[coercion_enabled]] 
-declare [[coercion "int :: nat \<Rightarrow> int"]]
-
-fun inth :: "'a list \<Rightarrow> int \<Rightarrow> 'a" (infixl "!!" 100) where
-  "(x # xs) !! i = (if i = 0 then x else xs !! (i - 1))"
-
-lemma inth_append [simp]:
-  "0 \<le> i \<Longrightarrow>
-  (xs @ ys) !! i = (if i < size xs then xs !! i else ys !! (i - size xs))"
-by (induction xs arbitrary: i) (auto simp: algebra_simps)
-
 type_synonym vname = string
 type_synonym val = int
 type_synonym state = "vname \<Rightarrow> val"
@@ -39,7 +27,7 @@ definition is_halt :: "config \<Rightarrow> bool" where
 fun iexec :: "instr \<Rightarrow> config \<Rightarrow> config" where
   "iexec instr (p, s) = (
     case instr of
-      HALT \<Rightarrow> (p, s) |
+      HALT \<Rightarrow> (0, s) |
       x ::= t \<Rightarrow> (p + 1, case t of N c \<Rightarrow> s(x := c) | V y \<Rightarrow> s(x := s y)) |
       x += t \<Rightarrow> (p + 1, case t of N c \<Rightarrow> s(x := s x + c) | V y \<Rightarrow> s(x := s x + s y)) | 
       x -= t \<Rightarrow> (p + 1, case t of N c \<Rightarrow> s(x := s x - c) | V y \<Rightarrow> s(x := s x - s y)) | 
@@ -50,10 +38,10 @@ fun iexec :: "instr \<Rightarrow> config \<Rightarrow> config" where
   )"
 
 definition exec1 :: "GOTO_prog \<Rightarrow> config \<Rightarrow> config \<Rightarrow> bool" ("(_/ \<turnstile> (_ \<rightarrow>/ _))" [59,0,59] 60) where
-  "P \<turnstile> c \<rightarrow> c' = (\<exists>p s. c = (p, s) \<and> c' = iexec (P !! (p - 1)) c \<and> 0 < p \<and> p \<le> size P)"
+  "P \<turnstile> c \<rightarrow> c' = (\<exists>p s. c = (p, s) \<and> c' = iexec (P ! (p - 1)) c \<and> 0 < p \<and> p \<le> size P)"
 
 lemma exec1I [intro, code_pred_intro]:
-  "c' = iexec (P !! (p - 1)) (p, s) \<Longrightarrow> 0 < p \<Longrightarrow> p \<le> size P \<Longrightarrow> P \<turnstile> (p, s) \<rightarrow> c'"
+  "c' = iexec (P ! (p - 1)) (p, s) \<Longrightarrow> 0 < p \<Longrightarrow> p \<le> size P \<Longrightarrow> P \<turnstile> (p, s) \<rightarrow> c'"
   by (simp add: exec1_def of_nat_diff)
 
 abbreviation 
@@ -65,12 +53,10 @@ lemmas exec_induct = star.induct [of "exec1 P", split_format(complete)]
 
 code_pred exec1 using exec1I exec1_def by auto
 
-(*
+text \<open>A small example for z = x + y\<close>
 values
-  "{(p, map t [''x'',''y'']) | p t. (
-    [''x'' ::= N 3, ''z'' ::= V ''x'', HALT] \<turnstile>
-    (1, (\<lambda>x. 0)(''x'' := 3)) \<rightarrow>* (p, t))}"
+  "{(p, map t [''x'', ''y'', ''z'']) | p t. (
+    [''x'' ::= N 3, ''y'' ::= N 4, ''z'' ::= V ''x'', ''z'' += V ''y'', HALT] \<turnstile>
+    (1, (\<lambda>x. 0)) \<rightarrow>* (p, t))}"
 
-  Warning: do not terminate. Why?
-*)
 end
