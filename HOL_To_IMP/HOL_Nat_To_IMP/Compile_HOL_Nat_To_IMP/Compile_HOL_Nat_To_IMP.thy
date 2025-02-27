4eb475f9e77c6ca2b425fbfe0db50a8c5213f019
Nico Lintner <118087966+nlintn@users.noreply.github.com>
2025-02-27 14:35:05 +0100

    improved trans_assigns, tuned proof

--- HOL_To_IMP/HOL_Nat_To_IMP/Compile_HOL_Nat_To_IMP/Compile_HOL_Nat_To_IMP.thy
+++ HOL_To_IMP/HOL_Nat_To_IMP/Compile_HOL_Nat_To_IMP/Compile_HOL_Nat_To_IMP.thy
@@ -137,8 +137,14 @@ fun subst_atomExp :: "atomExp \<Rightarrow> (vname \<times> atomExp) list \<Righ
 
 fun subst_aexp :: "aexp \<Rightarrow> (vname \<times> atomExp) list \<Rightarrow> aexp" where
   "subst_aexp (A a) al = A (subst_atomExp a al)"
-| "subst_aexp (Plus a1 a2) al = Plus (subst_atomExp a1 al) (subst_atomExp a2 al)"
-| "subst_aexp (Sub a1 a2) al = Sub (subst_atomExp a1 al) (subst_atomExp a2 al)"
+| "subst_aexp (Plus a1 a2) al = (case (subst_atomExp a1 al, subst_atomExp a2 al) of
+    (N n1, N n2) \<Rightarrow> A (N (n1 + n2)) |
+    (N 0, V v) \<Rightarrow> A (V v) | (V v, N 0) \<Rightarrow> A (V v) |
+    (a1', a2') \<Rightarrow> Plus a1' a2')"
+| "subst_aexp (Sub a1 a2) al = (case (subst_atomExp a1 al, subst_atomExp a2 al) of
+    (N n1, N n2) \<Rightarrow> A (N (n1 - n2)) |
+    (N 0, V v) \<Rightarrow> A (N 0) | (V v, N 0) \<Rightarrow> A (V v) |
+    (a1', a2') \<Rightarrow> Sub a1' a2')"
 
 definition
   "rm_assn_var v \<equiv> filter (\<lambda>(_, v'). v' \<noteq> V v)"
@@ -153,7 +159,8 @@ fun trans_assigns_aux :: "tcom \<Rightarrow> (vname \<times> atomExp) list \<Rig
     let
       (c1', al'1) = trans_assigns_aux c1 al;
       (c2', al'2) = trans_assigns_aux c2 al
-    in (tIf (case get_reg al v of (V v') \<Rightarrow> v' | _ \<Rightarrow> v) c1' c2', filter (List.member al'2) al'1))"
+    in (case get_reg al v of V v' \<Rightarrow> (tIf v' c1' c2', filter (List.member al'2) al'1) |
+          N 0 \<Rightarrow> (tSeq tSKIP c2', al'2) | N n \<Rightarrow> (tSeq tSKIP c1', al'1)))"
 | "trans_assigns_aux (tAssign v a) al = (let a' = subst_aexp a al
     in (tAssign v a',
       rm_assn_var v (case a' of A atom \<Rightarrow> AList.update v atom al | _ \<Rightarrow> AList.delete_aux v al)))"
@@ -168,83 +175,93 @@ lemma al_delete_keep_invar:
   shows "\<And>x y. (x, y) \<in> set (rm_assn_var v (AList.delete_aux v al)) \<Longrightarrow>
     (s (v := a)) x = atomVal y (s (v := a))"
   using assms by (auto simp: rm_assn_var_def set_delete_aux)
-    (metis fun_upd_other atomExp.exhaust atomVal.simps(1,2))
+    (metis fun_upd_other atomExp.exhaust atomVal.simps)
 
 lemma atomVal_get_reg_eq_atomVal:
   assumes "\<And>x y. (x, y) \<in> set al \<Longrightarrow> s x = atomVal y s"
   shows "atomVal (get_reg al v) s = atomVal (V v) s"
   using assms by (induction al) (fastforce simp: get_reg_def)+
 
+lemma atomVal_subst_atomExp_eq_atomVal:
+  assumes "\<And>x y. (x, y) \<in> set al \<Longrightarrow> s x = atomVal y s"
+  shows "atomVal (subst_atomExp a al) s = atomVal a s"
+  using atomVal_get_reg_eq_atomVal[OF assms] by (cases a) simp_all
+
 lemma aval_subst_aexp_eq_aval:
   assumes "\<And>x y. (x, y) \<in> set al \<Longrightarrow> s x = atomVal y s"
   shows "aval (subst_aexp a al) s = aval a s"
-  apply (cases a)
-  subgoal for a by (cases a) (simp_all add: assms atomVal_get_reg_eq_atomVal)
-  subgoal for a1 a2 by (cases a1; cases a2) (simp_all add: assms atomVal_get_reg_eq_atomVal)
-  subgoal for a1 a2 by (cases a1; cases a2) (simp_all add: assms atomVal_get_reg_eq_atomVal)
-  done
+  using atomVal_subst_atomExp_eq_atomVal[OF assms] atomVal_get_reg_eq_atomVal[OF assms]
+  by (cases a) 
+    (auto simp: nat.case_eq_if split: atomExp.split;
+      metis add_cancel_left_left add_cancel_left_right atomVal.simps less_eq_nat.simps(1) diff_zero)+
+
 
 lemma trans_assigns_aux_al_distinct:
   assumes "(c', al') = trans_assigns_aux c al" "distinct (map fst al)"
   shows "distinct (map fst al')"
   using assms by (induction c al arbitrary: c' al' rule: trans_assigns_aux.induct)
     (auto simp: distinct_map_filter distinct_update distinct_delete rm_assn_var_def Let_def
-      split: prod.splits aexp.split)
+      split: prod.splits aexp.split atomExp.splits nat.splits)
 
 lemma tbig_step_trans_assigns_aux1:
   includes tcom_syntax
   assumes "(c', al') = trans_assigns_aux c al"
     and "C \<turnstile> (c, s) \<Rightarrow>\<^bsup>t\<^esup> s'"
     and "\<And>x y. (x, y) \<in> set al \<Longrightarrow> s x = atomVal y s" "distinct (map fst al)"
-  shows "C \<turnstile> (c', s) \<Rightarrow>\<^bsup>t\<^esup> s'" "\<And>x y. ((x, y) \<in> set al' \<longrightarrow> s' x = atomVal y s')"
+  shows "C \<turnstile> (c', s) \<Rightarrow>\<^bsup>t\<^esup> s'" "\<And>x y. ((x, y) \<in> set al' \<Longrightarrow> s' x = atomVal y s')"
 proof -
-  from assms have "\<And>x y. C \<turnstile> (c', s) \<Rightarrow>\<^bsup>t\<^esup> s' \<and> ((x, y) \<in> set al' \<longrightarrow> s' x = atomVal y s')"
+  from assms have "C \<turnstile> (c', s) \<Rightarrow>\<^bsup>t\<^esup> s' \<and> (\<forall>(x, y) \<in> set al'. s' x = atomVal y s')"
   proof (induction c al arbitrary: c' al' s t s' rule: trans_assigns_aux.induct)
     case (1 c1 c2 al)
-    from 1(3) obtain c1' al1 where c1': "(c1', al1) = trans_assigns_aux c1 al"
-      by (simp add: split_pairs)
+    from 1(3) obtain c1' al1 c2' where prod: "(c1', al1) = trans_assigns_aux c1 al"
+        "(c2', al') = trans_assigns_aux c2 al1"
+      by (simp split: prod.splits)
     obtain t1 s1 t2 where obt: "C \<turnstile> (c1, s) \<Rightarrow>\<^bsup>t1\<^esup>  s1" "C \<turnstile> (c2, s1) \<Rightarrow>\<^bsup>t2\<^esup>  s'" "t1 + t2 = t"
       using 1(4) by blast
     have left: "C \<turnstile> (c1', s) \<Rightarrow>\<^bsup>t1\<^esup>  s1" "\<And>x y. (x, y) \<in> set al1 \<Longrightarrow> s1 x = atomVal y s1"
-      using 1(1)[OF c1' obt(1) 1(5,6)] by blast+
-    from 1(3) c1' obtain c2' where c2': "(c2', al') = trans_assigns_aux c2 al1"
-      by (simp split: prod.splits)
+      using 1(1)[OF prod(1) obt(1) 1(5,6)] by blast+
     have right: "C \<turnstile> (c2', s1) \<Rightarrow>\<^bsup>t2\<^esup>  s'" "\<And>x y. (x, y) \<in> set al' \<Longrightarrow> s' x = atomVal y s'"
-      using 1(2)[OF c1' refl c2' obt(2) left(2) trans_assigns_aux_al_distinct[OF c1' 1(6)]] by blast+
-    from left right obt(3) 1(3) c1' c2' show ?case by (auto split: prod.splits)
+      using 1(2)[OF prod(1) refl prod(2) obt(2) left(2)
+          trans_assigns_aux_al_distinct[OF prod(1) 1(6)]] by blast+
+    from left right obt(3) 1(3) prod show ?case by (auto split: prod.splits)
   next                               
     case (2 v c1 c2 al)
-    obtain c2' al2 where obt2: "(c2', al2) = trans_assigns_aux c2 al" by (simp add: split_pairs)
     have v': "s (case get_reg al v of V v' \<Rightarrow> v' | _ \<Rightarrow> v) = s v" 
       using atomVal_get_reg_eq_atomVal[OF 2(5), of al v] by (auto split: atomExp.split)
     show ?case
     proof (cases "s v = 0")
       case True
-      with 2(4)[THEN tIf_tE] obtain t' where t': "C \<turnstile> (c2, s) \<Rightarrow>\<^bsup>t'\<^esup> s'" "t = t' + 1"
+      with 2(4)[THEN tIf_tE] obtain t' where t': "C \<turnstile> (c2, s) \<Rightarrow>\<^bsup>t'\<^esup> s'" "t = t' + 1" "t = Suc 0 + t'"
         using bot_nat_0.not_eq_extremum by auto
       obtain c2' al2 where obt2: "(c2', al2) = trans_assigns_aux c2 al" by (simp add: split_pairs)
-      have 1: "C \<turnstile> (c2', s) \<Rightarrow>\<^bsup>t'\<^esup>  s'" "\<And>x y. (x, y) \<in> set al2 \<longrightarrow> s' x = atomVal y s'"
+      have 1: "C \<turnstile> (c2', s) \<Rightarrow>\<^bsup>t'\<^esup>  s'" "\<And>x y. (x, y) \<in> set al2 \<Longrightarrow> s' x = atomVal y s'"
         using 2(2)[OF _ refl obt2 t'(1) 2(5,6)] prod.collapse by blast+
-      from obt2 True 2(3) have "set al' \<subseteq> set al2"
-        by (auto simp: member_def split: prod.splits)
-      with 1(2) have "\<And>x y. (x, y) \<in> set al' \<longrightarrow> s' x = atomVal y s'" by blast
-      moreover have "C \<turnstile> (c', s) \<Rightarrow>\<^bsup>t\<^esup>  s'" 
-        using tIfFalse[OF True[folded v'] 1(1) t'(2)] 2(3) obt2
-        by (auto split: prod.splits)
+      have case_dest: "case get_reg al v of N n \<Rightarrow> n = 0 | V v' \<Rightarrow> s v' = 0"
+        using True obt2 2(3) atomVal_get_reg_eq_atomVal[OF 2(5), of al v]
+        by (simp split: prod.splits atomExp.splits nat.splits)
+      with 2(3) obt2 have "set al' \<subseteq> set al2"
+        by (auto split: prod.splits atomExp.splits simp: member_def)
+      with 1(2) have "\<And>x y. (x, y) \<in> set al' \<Longrightarrow> s' x = atomVal y s'" by blast
+      moreover from True case_dest 2(3) obt2 have "C \<turnstile> (c', s) \<Rightarrow>\<^bsup>t\<^esup>  s'" 
+        using tIfFalse[OF True[folded v'] 1(1) t'(2)] tSeq[OF tSkip t'(1) t'(3)]
+        by (fastforce split: prod.splits atomExp.splits nat.splits) 
       ultimately show ?thesis by blast
     next
       case False
-      with 2(4)[THEN tIf_tE] obtain t' where t': "C \<turnstile> (c1, s) \<Rightarrow>\<^bsup>t'\<^esup> s'" "t = t' + 1"
+      with 2(4)[THEN tIf_tE] obtain t' where t': "C \<turnstile> (c1, s) \<Rightarrow>\<^bsup>t'\<^esup> s'" "t = t' + 1" "t = Suc 0 + t'"
         using bot_nat_0.not_eq_extremum by auto
       obtain c1' al1 where obt1: "(c1', al1) = trans_assigns_aux c1 al" by (simp add: split_pairs)
-      have 1: "C \<turnstile> (c1', s) \<Rightarrow>\<^bsup>t'\<^esup>  s'" "\<And>x y. (x, y) \<in> set al1 \<longrightarrow> s' x = atomVal y s'"
+      have 1: "C \<turnstile> (c1', s) \<Rightarrow>\<^bsup>t'\<^esup>  s'" "\<And>x y. (x, y) \<in> set al1 \<Longrightarrow> s' x = atomVal y s'"
         using 2(1)[OF obt1 t'(1) 2(5,6)] by blast+
-      from obt1 False 2(3) have "set al' \<subseteq> set al1"
-        by (auto simp: member_def split: prod.splits)
-      with 1(2) have "\<And>x y. (x, y) \<in> set al' \<longrightarrow> s' x = atomVal y s'" by blast
-      moreover have "C \<turnstile> (c', s) \<Rightarrow>\<^bsup>t\<^esup>  s'" 
-        using tIfTrue[OF False[folded v'] 1(1) t'(2)] 2(3) obt1
-        by (auto split: prod.splits)
+      have case_dest: "case get_reg al v of N n \<Rightarrow> n \<noteq> 0 | V v' \<Rightarrow> s v' \<noteq> 0"
+        using False obt1 2(3) atomVal_get_reg_eq_atomVal[OF 2(5), of al v]
+        by (simp split: prod.splits atomExp.splits nat.splits)
+      with obt1 2(3) have "set al' \<subseteq> set al1"
+        by (auto simp: member_def split: prod.splits atomExp.splits nat.splits)
+      with 1(2) have "\<And>x y. (x, y) \<in> set al' \<Longrightarrow> s' x = atomVal y s'" by blast
+      moreover from False case_dest 2(3) obt1 have "C \<turnstile> (c', s) \<Rightarrow>\<^bsup>t\<^esup>  s'" 
+        using tIfTrue[OF False[folded v'] 1(1) t'(2)] tSeq[OF tSkip t'(1) t'(3)]
+        by (fastforce split: prod.splits atomExp.splits nat.splits)
       ultimately show ?thesis by blast
     qed
   next
@@ -254,9 +271,8 @@ proof -
       using aval_subst_aexp_eq_aval[OF 3(3)] by blast
     with 3(1) t s' have "C \<turnstile> (c', s) \<Rightarrow>\<^bsup>t\<^esup> s'"
       using tAssign[of C v "subst_aexp a al" s] by (simp add: split_pairs Let_def)
-    moreover have "(x, y) \<in> set al' \<Longrightarrow> s' x = atomVal y s'"
+    moreover have "s' x = atomVal y s'" if asm: "(x, y) \<in> set al'" for x y
     proof -
-      assume asm: "(x, y) \<in> set al'"
       have s': "s' = s (v := aval a s)"
         using 3(1,2) by auto
       from asm 3(1) have "y \<noteq> (V v)"
@@ -301,7 +317,7 @@ proof -
     case (4 c v al)
     from 4(1,2) al_delete_keep_invar[OF 4(3,4)] show ?case by auto
   qed auto
-  then show "C \<turnstile> (c', s) \<Rightarrow>\<^bsup>t\<^esup> s'" "\<And>x y. ((x, y) \<in> set al' \<longrightarrow> s' x = atomVal y s')" by blast+
+  then show "C \<turnstile> (c', s) \<Rightarrow>\<^bsup>t\<^esup> s'" "\<And>x y. ((x, y) \<in> set al' \<Longrightarrow> s' x = atomVal y s')" by blast+
 qed
 
 lemma tbig_step_trans_assigns_aux2:
@@ -309,41 +325,63 @@ lemma tbig_step_trans_assigns_aux2:
   assumes "(c', al') = trans_assigns_aux c al"
     and "C \<turnstile> (c', s) \<Rightarrow>\<^bsup>t\<^esup> s'"
     and "\<And>x y. (x, y) \<in> set al \<Longrightarrow> s x = atomVal y s" "distinct (map fst al)"
-  shows "C \<turnstile> (c, s) \<Rightarrow>\<^bsup>t\<^esup> s'" "\<And>x y. ((x, y) \<in> set al' \<longrightarrow> s' x = atomVal y s')"
+  shows "C \<turnstile> (c, s) \<Rightarrow>\<^bsup>t\<^esup> s'" "\<And>x y. ((x, y) \<in> set al' \<Longrightarrow> s' x = atomVal y s')"
 proof -
-  from assms have "\<And>x y. C \<turnstile> (c, s) \<Rightarrow>\<^bsup>t\<^esup> s' \<and> ((x, y) \<in> set al' \<longrightarrow> s' x = atomVal y s')"
+  from assms have "C \<turnstile> (c, s) \<Rightarrow>\<^bsup>t\<^esup> s' \<and> (\<forall>(x, y) \<in> set al'. s' x = atomVal y s')"
   proof (induction c al arbitrary: c' al' s t s' rule: trans_assigns_aux.induct)
     case (1 c1 c2 al)
-    from 1(3) obtain c1' al1 where c1': "(c1', al1) = trans_assigns_aux c1 al"
-      by (simp add: split_pairs)
-    with 1(3) obtain c2' where c2': "(c2', al') = trans_assigns_aux c2 al1"
+    from 1(3) obtain c1' al1 c2' where prod: "(c1', al1) = trans_assigns_aux c1 al"
+        "(c2', al') = trans_assigns_aux c2 al1"
       by (simp split: prod.splits)
     obtain t1 s1 t2 where obt: "C \<turnstile> (c1', s) \<Rightarrow>\<^bsup>t1\<^esup>  s1" "C \<turnstile> (c2', s1) \<Rightarrow>\<^bsup>t2\<^esup>  s'" "t1 + t2 = t"
-      using c1' c2' 1(3,4) by (auto split: prod.splits)
+      using prod 1(3,4) by (auto split: prod.splits)
     have left: "C \<turnstile> (c1, s) \<Rightarrow>\<^bsup>t1\<^esup>  s1" "\<And>x y. (x, y) \<in> set al1 \<Longrightarrow> s1 x = atomVal y s1"
-      using 1(1)[OF c1' obt(1) 1(5,6)] by blast+
+      using 1(1)[OF prod(1) obt(1) 1(5,6)] by blast+
     have right: "C \<turnstile> (c2, s1) \<Rightarrow>\<^bsup>t2\<^esup>  s'" "\<And>x y. (x, y) \<in> set al' \<Longrightarrow> s' x = atomVal y s'"
-      using 1(2)[OF c1' refl c2' obt(2) left(2) trans_assigns_aux_al_distinct[OF c1' 1(6)]] by blast+
-    from left right obt(3) 1(3) c1' c2' show ?case by (auto split: prod.splits)
+      using 1(2)[OF prod(1) refl prod(2) obt(2) left(2)
+          trans_assigns_aux_al_distinct[OF prod(1) 1(6)]] by blast+
+    from left right obt(3) 1(3) prod show ?case by (auto split: prod.splits)
   next
     case (2 v c1 c2 al)
-    have v_same: "s (case get_reg al v of (V v') \<Rightarrow> v' | _ \<Rightarrow> v) = s v" (is "s ?v' = _")
+    have v_same: "(case get_reg al v of (V v') \<Rightarrow> s v' | (N n) \<Rightarrow> n) = s v"
       using atomVal_get_reg_eq_atomVal[OF 2(5), of al v] by (auto split: atomExp.splits)
-    have c': "c' = IF ?v'\<noteq>0 THEN (fst (trans_assigns_aux c1 al)) ELSE (fst (trans_assigns_aux c2 al))"
-      using 2(3) by (simp split: prod.splits)
     have "C \<turnstile> (IF v\<noteq>0 THEN c1 ELSE c2, s) \<Rightarrow>\<^bsup>t\<^esup>  s'"
-    proof (cases "s v = 0")
-      case True
-      then obtain t' where "C \<turnstile> (fst (trans_assigns_aux c2 al), s) \<Rightarrow>\<^bsup>t'\<^esup>  s'" "t = t' + 1"
-        using 2(4)[unfolded c', THEN tIf_tE] unfolding v_same by auto
-      then show ?thesis
-        using tIfFalse[of s, OF True] 2(2) 2(5,6) by (simp add: split_pairs)
+    proof (cases "get_reg al v")
+      case (N n)
+      show ?thesis
+      proof (cases "n = 0")
+        case True
+        with v_same 2(3,4) N have c': "c' = tSKIP;;fst (trans_assigns_aux c2 al)" 
+          by (auto split: prod.splits atomExp.splits nat.splits)
+        then obtain t' where "C \<turnstile> (fst (trans_assigns_aux c2 al), s) \<Rightarrow>\<^bsup>t'\<^esup>  s'" "t = t' + 1"
+          using 2(4)[unfolded c'] by auto
+        with True tIfFalse[of s v] v_same 2(2) 2(5,6) N show ?thesis by (simp add: split_pairs)
+      next
+        case False
+        with v_same 2(3,4) N have c': "c' = tSKIP;;fst (trans_assigns_aux c1 al)" 
+          by (auto split: prod.splits atomExp.splits nat.splits)
+        then obtain t' where "C \<turnstile> (fst (trans_assigns_aux c1 al), s) \<Rightarrow>\<^bsup>t'\<^esup>  s'" "t = t' + 1"
+          using 2(4)[unfolded c'] by auto
+        with False tIfTrue[of s v] v_same 2(1) 2(5,6) N show ?thesis by (simp add: split_pairs)
+      qed
     next
-      case False
-      then obtain t' where "C \<turnstile> (fst (trans_assigns_aux c1 al), s) \<Rightarrow>\<^bsup>t'\<^esup>  s'" "t = t' + 1"
-        using 2(4)[unfolded c', THEN tIf_tE] unfolding v_same by auto
-      then show ?thesis
-        using tIfTrue[of s, OF False] 2(1) 2(5,6) by (simp add: split_pairs)
+      case (V v')
+      with 2(3) have c': "c' = IF v'\<noteq>0 THEN (fst (trans_assigns_aux c1 al)) ELSE (fst (trans_assigns_aux c2 al))"
+        by (auto split: prod.splits atomExp.splits nat.splits)
+      show ?thesis
+      proof (cases "s v = 0")
+        case True
+        then obtain t' where "C \<turnstile> (fst (trans_assigns_aux c2 al), s) \<Rightarrow>\<^bsup>t'\<^esup>  s'" "t = t' + 1"
+          using V 2(4)[unfolded c', THEN tIf_tE] v_same by auto
+        then show ?thesis
+          using tIfFalse[of s, OF True] 2(2) 2(5,6) by (simp add: split_pairs)
+      next
+        case False
+        then obtain t' where "C \<turnstile> (fst (trans_assigns_aux c1 al), s) \<Rightarrow>\<^bsup>t'\<^esup>  s'" "t = t' + 1"
+          using V 2(4)[unfolded c', THEN tIf_tE] v_same by auto
+        then show ?thesis
+          using tIfTrue[of s, OF False] 2(1) 2(5,6) by (simp add: split_pairs)
+      qed
     qed
     with tbig_step_trans_assigns_aux1(2)[OF 2(3) this 2(5,6)] show ?case by blast
   next
@@ -353,9 +391,9 @@ proof -
     with tbig_step_trans_assigns_aux1(2)[OF 3(1) this 3(3,4)] show ?case by blast
   next
     case (4 c v al)
-    from tbig_step_trans_assigns_aux1(2)[OF 4(1) _ 4(3,4)] 4(1,2) show ?case by simp
+    from tbig_step_trans_assigns_aux1(2)[OF 4(1) _ 4(3,4)] 4(1,2) show ?case by fastforce
   qed auto
-  then show "C \<turnstile> (c, s) \<Rightarrow>\<^bsup>t\<^esup> s'" "\<And>x y. ((x, y) \<in> set al' \<longrightarrow> s' x = atomVal y s')" by blast+
+  then show "C \<turnstile> (c, s) \<Rightarrow>\<^bsup>t\<^esup> s'" "\<And>x y. ((x, y) \<in> set al' \<Longrightarrow> s' x = atomVal y s')" by blast+
 qed
 
 lemma tbig_step_trans_assigns_aux_iff:
